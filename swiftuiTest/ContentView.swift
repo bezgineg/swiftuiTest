2c2627fcd8c57fd8574fa6b35a9256b319e9d581
BezginEvgenii <bezgineg@yandex.ru>
2021-07-30 17:54:09 +0200

    Finish task one and two

--- swiftuiTest/ContentView.swift
+++ swiftuiTest/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 16) {
+            Text("Это заголовок")
+                .modifier(Title())
+            Text("Это основной текст")
+                .modifier(Description())
+        }
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .background(Color.orange)
+            .font(.system(size: 50, weight: .bold, design: .default))
+    }
+}
+
+struct Description: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 20, weight: .regular, design: .default))
     }
 }
 
@@ -19,3 +39,64 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+/*struct Decision: Identifiable {
+    
+    var id: String {
+        string
+    }
+    let string: String
+}
+
+struct ContentView: View {
+    @State var decision: Decision?
+    
+    var body: some View {
+        VStack(spacing: 16) {
+           Text("To be or not to be?")
+            Button("To be") {
+                decision = .init(string: "to be")
+            }
+            Button("Not to be") {
+                decision = .init(string: "not to be")
+            }
+        }
+        .alert(item: $decision) { decision in
+            Alert(title: Text(decision.string), message: Text("Poor Yorik"), dismissButton: .cancel())
+        }
+    }
+}*/
+
+/*struct ContentView: View {
+    
+    @State var speed = 50.0
+    @State var isEditing = false
+    
+    var body: some View {
+        VStack {
+            Slider(value: $speed, in: 0...100) { isEditing in
+                self.isEditing = isEditing
+            }
+            Text("\(speed)")
+                .foregroundColor(isEditing ? .red : .green)
+        }
+    }
+}*/
+
+/*struct ContentView: View {
+    
+    @State var notificationEnabled = false
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Toggle(isOn: $notificationEnabled, label: {
+                        Text("Notification Enabled")
+                    })
+                }
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}*/
