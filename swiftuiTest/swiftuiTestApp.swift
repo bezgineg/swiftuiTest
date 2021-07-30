207c55810f67e78b97409a5ddad76b21c1c8771c
BezginEvgenii <bezgineg@yandex.ru>
2021-07-30 17:14:38 +0200

    first commit

---
+++ swiftuiTest/swiftuiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiTestApp.swift
+//  swiftuiTest
+//
+//  Created by user196284 on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
